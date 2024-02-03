10b7e9dade28b63f0ec4ff1be9d405c80e7092ad
dgusfr <diegogustavo.fr@gmail.com>
2024-02-03 09:12:31 -0300

    commit

--- mp.py
+++ mp.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url_base = 'https://lista.mercadolivre.com.br/'
+url_base = 'https://www.mercadolivre.com.br/'
 
 produto_nome = input('Qual produto você deseja? ')
 
@@ -9,23 +9,17 @@ response = requests.get(url_base + produto_nome)
 
 site = BeautifulSoup(response.text, 'html.parser')
 
-produtos = site.findAll('div', attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result ui-search-result--core andes-card--padding-default'})
+produtos = site.findAll('a', attrs={'class': 'ui-search-item__group__element ui-search-link__title-card ui-search-link'})
+
+if not produtos:
+    print("Nenhuma classe encontrada. Verifique a classe ou a estrutura do HTML.")
 
 for produto in produtos:
     titulo = produto.find('h2', attrs={'class': 'ui-search-item__title'})
-    link = produto.find('a', attrs={'class': 'ui-search-link'})
-
-    real = produto.find('span', attrs={'class': 'price-tag-fraction'})
-    centavos = produto.find('span', attrs={'class': 'price-tag-cents'})
+    real = produto.find('span', attrs={'class': 'andes-money-amount__fraction'})
+    
 
     print(produto.prettify())
     print('Título do produto:', titulo.text)
-    print('Link do produto:', link['href'])
-
-    if (centavos):
-        print('Preço do produto: R$', real.text + ',' + centavos.text)
-    else:
-        print('Preço do produto: R$', real.text)
-    
     print('\n\n')
     break
